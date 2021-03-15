3ba2ea545abcd70581aa0dd956d219478eacbaca
Abhiraj Mhetre <55054832+AbhirajMhetre@users.noreply.github.com>
2021-03-15 12:15:52 +0530

    send all products

--- controllers/productController.js
+++ controllers/productController.js
@@ -8,24 +8,10 @@ import { notFound, errorHandler } from '../middleware/errorMiddleware.js'
 const getProducts = asyncHandler(async (req, res) => {
   try{
 
-  const pageSize = 10
-  const page = Number(req.query.pageNumber) || 1
+  const products = await Product.find()
 
-  const keyword = req.query.keyword
-    ? {
-        name: {
-          $regex: req.query.keyword,
-          $options: 'i',
-        },
-      }
-    : {}
 
-  const count = await Product.countDocuments({ ...keyword })
-  const products = await Product.find({ ...keyword })
-    .limit(pageSize)
-    .skip(pageSize * (page - 1))
-
-  res.json({ products, page, pages: Math.ceil(count / pageSize) })
+  res.json(products)
   } catch (err) {
       console.error(err.message);
       errorHandler(err, req, res);
