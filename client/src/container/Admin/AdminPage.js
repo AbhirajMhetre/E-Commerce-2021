bfe89711a646f51f1880ffb56681fcd57940dd2e
Vivek <vivekdmistry1999@gmail.com>
2021-03-19 10:05:52 +0530

    Creating Saperate Navbar and header file for admin

--- client/src/container/Admin/AdminPage.js
+++ client/src/container/Admin/AdminPage.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 import './AdminPage.css';
-import AdminHeader from '../../components/Header/AdminHeader/AdminHeader';
+import AdminHeader from '../../components/Admin/AdminHeader/AdminHeader';
 import AdminProducts from '../../components/Admin/AdminProducts/AdminProducts';
-import Navbar from '../../components/Navbar/Navbar';
+import AdminNavbar from '../../components/Admin/AdminNavbar/AdminNavbar';
 
 const AdminPage = () => {   
     return(
         <>
-            <Navbar/>
+            <AdminNavbar/>
             <div className="admin-page">
                 <AdminHeader/>
                 <AdminProducts/>
